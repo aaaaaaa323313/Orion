bd66ddb4844caebe5c539cba8ad720f7e0530737
springlake <544282962@qq.com>
2016-02-10 22:06:46 -0800

    a

--- tools/split_video.py
+++ tools/split_video.py
@@ -1,7 +1,14 @@
 import os
 
 video_path = '/home/guanyu/Socrates/new_data_set/'
+dst_path = '/home/guanyu/Socrates/data_set/'
 
 for file in os.listdir(video_path):
     if file.endswith(".mp4"):
-        print(file)
+        f_name = video_path + file
+        f_pre, _ = os.path.splitext(file)
+        print f_pre
+
+        #cmd = 'ffmpeg -i ' + f_name + ' -f segment -segment_time 10 -c copy -map 0 -segment_list ' + \
+        #    50mb.list 50mb%03d.mp4
+
