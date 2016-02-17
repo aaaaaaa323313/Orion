0bbf8c418475d7fa2a61930b6e0bd4a564ad94de
springlake <544282962@qq.com>
2016-02-17 01:14:57 -0800

    new

--- get_data.py
+++ get_data.py
@@ -1,3 +1,21 @@
+import json
+import time
+import urllib2
 
 
+f = open('trace.dat','w')
 
+while True:
+    try:
+        content = urllib2.urlopen("https://api.twitch.tv/kraken/streams/summary").read()
+        x = json.loads(content)
+        res = str(time.time()) + '      ' + str(x['channels'])
+        f.write(res)
+        f.flush()
+        time.sleep(60)
+
+    except:
+        print 'error'
+        continue
+
+f.close()
