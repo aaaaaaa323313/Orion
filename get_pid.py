6103bd44fd19e0f2aea40da0c58ecc54327ee022
springlake <544282962@qq.com>
2016-03-24 20:22:10 -0700

    new

---
+++ get_pid.py
@@ -0,0 +1,30 @@
+from subprocess import check_output
+import psutil
+import signal
+import redis
+import time
+import os
+
+def get_pid(name):
+    return check_output(["pidof",name])
+
+pid = get_pid('avconv')
+pid = int(pid)
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+cur_pid = r.get('cur_pid')
+cur_sta = r.get('cur_sta')
+
+if pid == cur_pid and cur_sta == 1:
+    os.kill(pid, signal.SIGSTOP)
+    r.set('cur_sta', 0)
+elif pid == cur_pid and cur_sta == 0:
+    pass
+elif pid != cur_pid:
+    os.kill(pid, signal.SIGSTOP)
+    r.set('cur_pid', pid)
+    r.set('cur_sta', 0)
+
+time.sleep(3);
+os.kill(pid, signal.SIGCONT)
+r.set('cur_sta', 1)
+
